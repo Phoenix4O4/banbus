1b1a8b4a091a2e649e2a1bac5b673378508a2757
Nick Farley <nick@nfreader.net>
2021-01-29 21:31:45 -0500

    The frontend now uses Tailwind! :D

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+     './views/**/*.twig',
+   ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    // require('@tailwindcss/typography'),
+  ],
+}

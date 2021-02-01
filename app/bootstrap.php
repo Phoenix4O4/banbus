0216c2dffcefc4d4ff4a892358d86e3c9d56b6ff
Nick Farley <nick@nfreader.net>
2021-01-31 19:19:36 -0500

    Basepath stuff

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -17,7 +17,15 @@ date_default_timezone_set($container->get('settings')['app']['timezone']);
 (require __DIR__ . '/middleware.php')($app);
 (require __DIR__ . '/routes.php')($app);
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
-if ($container->get('settings')['basepath']) {
-    $app->setBasePath($container->get('settings')['basepath']);
-}
+$app->setBasePath((function () {
+    $scriptDir = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
+    $uri = (string) parse_url('http://' . $_SERVER['REQUEST_URI'] ?? '', PHP_URL_PATH);
+    if (stripos($uri, $_SERVER['SCRIPT_NAME']) === 0) {
+        return $_SERVER['SCRIPT_NAME'];
+    }
+    if ($scriptDir !== '/' && stripos($uri, $scriptDir) === 0) {
+        return $scriptDir;
+    }
+    return '';
+})());
 return $app;

1da17af24d13e2b4a79aee1a171f814de781313e
Nick Farley <nick@nfreader.net>
2021-02-07 21:55:14 -0500

    Formatting updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,36 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: [
-    './views/**/*.twig',
-    './views/**/*.html',
-   ],
-  darkMode: 'media', // or 'media' or 'class'
+  purge: ["./views/**/*.twig", "./views/**/*.html"],
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Public Sans', ...defaultTheme.fontFamily.sans],
+        sans: ["Public Sans", ...defaultTheme.fontFamily.sans],
       },
     },
     fontSize: {
-        'xxs': '.25rem',
-       'xs': '.75rem',
-       'sm': '.875rem',
-       'tiny': '.875rem',
-        'base': '1rem',
-        'lg': '1.125rem',
-        'xl': '1.25rem',
-        '2xl': '1.5rem',
-       '3xl': '1.875rem',
-       '4xl': '2.25rem',
-        '5xl': '3rem',
-        '6xl': '4rem',
-       '7xl': '5rem',
-      }
+      xxs: ".25rem",
+      xs: ".75rem",
+      sm: ".875rem",
+      tiny: ".875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+      "7xl": "5rem",
+    },
   },
   variants: {
     extend: {
-      borderRadius: ['first', 'last']
-    }
+      borderRadius: ["first", "last"],
+    },
   },
   plugins: [
     // require('@tailwindcss/typography'),
   ],
-}
+};

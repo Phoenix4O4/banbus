68ef77728bc9b396f64c98dee59bab10d4cda2d1
Nick Farley <nick@nfreader.net>
2021-02-15 21:46:54 -0500

    Taking another swing at fixing CSS

--- app/version.php
+++ app/version.php
@@ -4,4 +4,4 @@ define('VERSION_MAJOR', 0);
 define('VERSION_MINOR', 5);
 define('VERSION_PATCH', 1);
 
-define('VERSION_TAG', null);
+define('VERSION_TAG', '-css-bump');
--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,19 @@
-const tailwind = require('tailwindcss')
-const purgecss = require('@fullhuman/postcss-purgecss')
-const cssnano = require('cssnano')
+const purgecss = require("@fullhuman/postcss-purgecss");
+const cssnano = require("cssnano");
 
 module.exports = {
   plugins: [
-    tailwind,
-    process.env.NODE_ENV === 'production' ? require('autoprefixer') : null,
-    process.env.NODE_ENV === 'production'
-      ? cssnano({ preset: 'default' })
+    require("postcss-import"),
+    require("tailwindcss"),
+    process.env.NODE_ENV === "production" ? require("autoprefixer") : null,
+    process.env.NODE_ENV === "production"
+      ? cssnano({ preset: "default" })
       : null,
-    process.env.NODE_ENV === 'production' ? purgecss({
-      content: [
-        './views/**/*.twig',
-        './views/**/*.html'
-      ],
-      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-    }) : null
-  ]
-}
+    process.env.NODE_ENV === "production"
+      ? purgecss({
+          content: ["./views/**/*.twig", "./views/**/*.html"],
+          defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        })
+      : null,
+  ],
+};

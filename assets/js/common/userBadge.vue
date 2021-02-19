5e592933f000c514b2da7d97fc3630682e05b265
Nick Farley <nick@nfreader.net>
2021-02-19 18:18:39 -0500

    Adds a user badge component, cleans up some styling

---
+++ assets/js/common/userBadge.vue
@@ -0,0 +1,23 @@
+<template>
+  <span
+    class="badge"
+    :style="{
+      backgroundColor: user.rank.backColor,
+      color: user.rank.foreColor,
+    }"
+  >
+    <i class="fa fa-fw" :class="['fa-' + user.rank.icon]"></i>
+    {{ user.displayName }}
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      required: true,
+      type: Object,
+    },
+  },
+};
+</script>
--- assets/js/ticketfeed/ticket.vue
+++ assets/js/ticketfeed/ticket.vue
@@ -1,25 +1,36 @@
 <template>
-  <div>
-    <button
-      v-on:click="mute"
-      class="bg-green-600 text-white block w-full p-2 text-xl font-bold mb-4 hover:bg-green-400 rounded"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <i
-        class="fa fa-fw"
-        :class="[muted ? 'fa-volume-mute' : 'fa-volume-up']"
-      ></i>
-      {{ muted ? "Unmute Sound" : "Mute Sound" }}
-    </button>
+  <section>
+    <h3 class="border-b border-gray-300 pb-4 mb-4 text-2xl font-bold">
+      Live Ticket Feed <i class="fas fa-circle text-red-500 animate-pulse"></i>
+    </h3>
+    <div class="flex itens-center mb-4">
+      <label class="inline-flex items-center">
+        <input type="checkbox" v-on:click="toggleNewTickets" />
+        <span class="mx-2 whitespace-nowrap">Only Show New Tickets</span>
+      </label>
+      <button
+        v-on:click="mute"
+        class="bg-green-600 text-white block w-full p-2 text-xl font-bold hover:bg-green-400 rounded"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i
+          class="fa fa-fw"
+          :class="[muted ? 'fa-volume-mute' : 'fa-volume-up']"
+        ></i>
+        {{ muted ? "Unmute Sound" : "Mute Sound" }}
+      </button>
+    </div>
     <p class="text-xs text-gray-300 text-center mb-4">{{ messages.text }}</p>
     <dl
       v-for="t in tickets"
       :key="t.id"
       :id="t.id"
-      class="flex mb-4 pb-4 border-b border-gray-300 ticket added"
+      class="flex mb-4 pb-4 border-b border-gray-300 ticket added transition"
     >
-      <dt class="whitespace-nowrap text-right pr-3 tabular-nums">
+      <dt
+        class="whitespace-nowrap text-right pr-3 tabular-nums border-r border-gray-300 mr-3"
+      >
         <a class="link" :href="'/tgdb/ticket/' + t.round + '/' + t.ticket"
           >#{{ t.round }}-{{ t.ticket }}</a
         >
@@ -28,24 +39,35 @@
         </span>
       </dt>
       <dd>
-        <span class="whitespace-nowrap">
+        <span class="whitespace-nowrap border-b border-gray-30 pb-2">
           <i
             class="fa fa-fw pr-3"
             :class="['fa-' + t.icon, 'text-' + t.class]"
           ></i>
-          <strong v-if="t.sender"> {{ t.sender.displayName }}</strong>
-          <strong v-else> {{ t.recipient.displayName }}</strong>
+          <span> {{ t.action }} by </span>
+          <userBadge v-if="t.sender" :user="t.sender" :key="t.id"></userBadge>
+          <span v-if="t.recipient">to </span>
+          <userBadge
+            v-if="t.recipient"
+            :user="t.recipient"
+            :key="t.id"
+          ></userBadge>
         </span>
-        {{ t.message }}
+        <p class="text-xl mt-2" v-html="t.message"></p>
       </dd>
     </dl>
-  </div>
+  </section>
 </template>
 
 <script>
 const initialTicketUrl = "?json=true";
 const pollUrl = "/tgdb/ticket/live/poll/?json=true";
+import userBadge from "./../common/userBadge.vue";
+
 export default {
+  components: {
+    userBadge,
+  },
   data() {
     return {
       tickets: [],
@@ -55,11 +77,20 @@ export default {
         type: "info",
         text: "Checking for new tickets...",
       },
+      newTickets: false,
     };
   },
   methods: {
     mute() {
       this.muted = !this.muted;
+    },
+    toggleNewTickets() {
+      this.newTickets = !this.newTickets;
+      if (this.newTickets) {
+        this.changeMessage("Only polling for newly opened tickets");
+      } else {
+        this.changeMessage("Polling for all ticket actions");
+      }
     },
     fetchInitialTickets() {
       fetch(initialTicketUrl, {
@@ -81,7 +112,10 @@ export default {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ lastId: this.lastId }),
+        body: JSON.stringify({
+          lastId: this.lastId,
+          newTickets: this.newTickets,
+        }),
       })
         .then((res) => res.json())
         .then((res) => {

50644c7a02c3d4daab5bf670aeafd36bd6a5e734
Nick Farley <nick@nfreader.net>
2021-02-27 12:36:48 -0500

    This probably saved me from horribly breaking prod yesterday

--- src/Repository/ConnectionFactory.php
+++ src/Repository/ConnectionFactory.php
@@ -9,9 +9,9 @@ class ConnectionFactory
     protected $db;
     protected $alt_db;
 
-    public function __construct(EasyDB $db, ?EasyDB $alt_db)
+    public function __construct(EasyDB $db, ?EasyDB $alt_db = null)
     {
         $this->db = $db;
-        $this->alt_db = $db;
+        $this->alt_db = $alt_db;
     }
 }

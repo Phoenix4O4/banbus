9bedd55816e0cef5bb4b4d6becfd3176ec8804f8
Nick Farley <nick@nfreader.net>
2021-02-27 14:00:53 -0500

    Fixes ckeysuggest not working

--- src/Domain/Player/Repository/PlayerRepository.php
+++ src/Domain/Player/Repository/PlayerRepository.php
@@ -2,13 +2,8 @@
 
 namespace App\Domain\Player\Repository;
 
-use ParagonIE\EasyDB\EasyDB;
 use App\Repository\Database;
 
 class PlayerRepository extends Database
 {
-    public function __construct(EasyDB $db)
-    {
-        parent::__construct($db);
-    }
 }

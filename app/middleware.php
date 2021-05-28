d811e449391a7b6a6bc0427b788ebe6318db7c21
Nick Farley <nick@nfreader.net>
2021-05-28 18:45:44 -0400

    Tweaking cookie settings

--- app/middleware.php
+++ app/middleware.php
@@ -3,8 +3,13 @@
 use Slim\App;
 use Slim\Views\TwigMiddleware;
 use App\Middleware\UrlGeneratorMiddleware;
+use Selective\SameSiteCookie\SameSiteCookieMiddleware;
+use Selective\SameSiteCookie\SameSiteCookieConfiguration;
 
 return function (App $app) {
+    $configuration = new SameSiteCookieConfiguration(['same_site' => 'strict']);
+
+    $app->add(new SameSiteCookieMiddleware($configuration));
     $app->addBodyParsingMiddleware();
     $app->add(UrlGeneratorMiddleware::class);
     $app->addRoutingMiddleware();

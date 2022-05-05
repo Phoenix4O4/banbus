71ed04644a0e4400c236131e3d84478207a4eb7e
Nick Farley <nick@nfreader.net>
2022-05-04 21:50:36 -0400

    Continuing work on #20

--- src/Domain/User/Repository/UserRepository.php
+++ src/Domain/User/Repository/UserRepository.php
@@ -8,16 +8,16 @@ final class UserRepository extends Database
 {
     public function getUserByCkey($ckey)
     {
-        return $this->db->row("SELECT p.ckey, a.rank, r.flags, a.feedback
+        return $this->db->row("SELECT p.ckey, SUBSTRING_INDEX(SUBSTRING_INDEX(a.rank, '+', 1), ',', -1) as `rank`, r.flags, a.feedback
         FROM `player` p
         LEFT JOIN `admin` a ON a.ckey = p.ckey
-        LEFT JOIN admin_ranks r ON a.rank = r.rank
+        LEFT JOIN admin_ranks r ON(FIND_IN_SET(a.rank, r.rank)
         WHERE p.ckey = ?", $ckey);
     }
 
     public function getUserByDiscordId(string $discordId)
     {
-        return $this->db->row("SELECT p.ckey, a.rank, r.flags, a.feedback
+        return $this->db->row("SELECT p.ckey, SUBSTRING_INDEX(SUBSTRING_INDEX(a.rank, '+', 1), ',', -1) as `rank`, r.flags, a.feedback
             FROM `player` p
             LEFT JOIN `admin` a ON p.ckey = a.ckey
             LEFT JOIN admin_ranks r ON a.rank = r.rank
